14feffc08d45f4b4db637a1e19e4cfc7ec3f8dd7
danishmahmoodawan <dani.ciitwah@gmail.com>
2022-02-08 05:52:40 +0500

    svg images changes check

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -336,7 +336,9 @@ to a community vote. Last but not least, we will live stream the winner selectio
  <div className="row">
                <div className="col-sm-12 pb-2">
                    <img src="images/regularlottery.png"  className="mt-5 deskpc"/>  
-                   <embed src="images/regularmob.svg" className="mt-2 tabmob" />
+                   <img src="images/special-mobile.png"  className="mt-5 tabmob"/>  
+                   <img src="images/specialmob.svg"  className="mt-5 tabmob"/>  
+
                    </div>
            </div>
 </div>
